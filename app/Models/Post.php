ab109e23c909487dff91b239be42aaeea382b7db
MuhirsanDev <muhirsan.id@gmail.com>
2024-03-04 11:35:54 +0700

    Create Setup Table and Model

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'content',
+        'status',
+    ];
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
