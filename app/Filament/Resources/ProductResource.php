d865629b11e9d555d2684b6b85b5cf1c47ddf73f
MuhirsanDev <muhirsan.id@gmail.com>
2024-03-07 20:25:02 +0700

    Changing the 'Products' menu icon

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -22,7 +22,7 @@ class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-building-storefront';
 
     public static function form(Form $form): Form
     {
